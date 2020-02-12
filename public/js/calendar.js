fcb2a6c4c0bfbf1d1d87fa3d8542e01d17e685c4
Jessica Nguyen <jdn039@ucsd.edu>
2020-02-12 11:26:00 -0800

    camera

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,5 +1,5 @@
 'use strict';
-
+var elementClicked;
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
   let current_date = new Date();
@@ -32,3 +32,46 @@ $(document).ready(function() {
     $("#month").text("APRIL");
   }
 })
+
+$("#calendarRow").find("span").click(function(){
+  $("#calendarRow span").removeClass("active-date");
+  $(this).addClass("active-date");
+});
+
+$(".chore-card .checkmark").click(function() {
+  $("#completeChore").modal("show");
+  elementClicked = this;
+});
+
+$("#confirm").click(function() {
+  $(elementClicked).css({"background-color": "#8e188a", "border-radius": "50%", "filter":"invert()"});
+});
+
+jQuery('img.svg').each(function(){
+    var $img = jQuery(this);
+    var imgID = $img.attr('id');
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
+
+    jQuery.get(imgURL, function(data) {
+        // Get the SVG tag, ignore the rest
+        var $svg = jQuery(data).find('svg');
+
+        // Add replaced image's ID to the new SVG
+        if(typeof imgID !== 'undefined') {
+            $svg = $svg.attr('id', imgID);
+        }
+        // Add replaced image's classes to the new SVG
+        if(typeof imgClass !== 'undefined') {
+            $svg = $svg.attr('class', imgClass+' replaced-svg');
+        }
+
+        // Remove any invalid XML tags as per http://validator.w3.org
+        $svg = $svg.removeAttr('xmlns:a');
+
+        // Replace image with new SVG
+        $img.replaceWith($svg);
+
+    }, 'xml');
+
+});
