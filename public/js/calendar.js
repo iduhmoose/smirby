0915e365690e85f893cbee98c9819af5710ab865
Jessica Nguyen <jdn039@ucsd.edu>
2020-02-13 13:50:15 -0800

    x

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,15 +1,18 @@
 'use strict';
 var elementClicked;
+var sunday;
+var current_date = new Date();
+var date = current_date.getDate(); // date
+var day = current_date.getDay(); // sun == 0
+var month = current_date.getMonth(); // jan starts at 0
+
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-  let current_date = new Date();
-  let date = current_date.getDate(); // date
-  let day = current_date.getDay(); // sun == 0
-  let month = current_date.getMonth(); // jan starts at 0
+
 
   let td_idx = day + 1;
 
-  let sunday = date - day;
+  sunday = date - day;
 
   for (let i = 1; i < 8; i++) {
     let id = "#calendarRow td:nth-child("+ i +") span";
@@ -31,6 +34,7 @@ $(document).ready(function() {
   } else if (month == 3) {
     $("#month").text("APRIL");
   }
+  sunday = date - day;
 })
 
 $("#calendarRow").find("span").click(function(){
@@ -47,31 +51,45 @@ $("#confirm").click(function() {
   $(elementClicked).css({"background-color": "#8e188a", "border-radius": "50%", "filter":"invert()"});
 });
 
-jQuery('img.svg').each(function(){
-    var $img = jQuery(this);
-    var imgID = $img.attr('id');
-    var imgClass = $img.attr('class');
-    var imgURL = $img.attr('src');
-
-    jQuery.get(imgURL, function(data) {
-        // Get the SVG tag, ignore the rest
-        var $svg = jQuery(data).find('svg');
-
-        // Add replaced image's ID to the new SVG
-        if(typeof imgID !== 'undefined') {
-            $svg = $svg.attr('id', imgID);
-        }
-        // Add replaced image's classes to the new SVG
-        if(typeof imgClass !== 'undefined') {
-            $svg = $svg.attr('class', imgClass+' replaced-svg');
-        }
-
-        // Remove any invalid XML tags as per http://validator.w3.org
-        $svg = $svg.removeAttr('xmlns:a');
-
-        // Replace image with new SVG
-        $img.replaceWith($svg);
-
-    }, 'xml');
-
+$(".remind").click(function() {
+  if ($(this).hasClass("reminded")) {
+    alert("You've already sent a reminder! Please wait 24 hours to send another one.");
+  }
+  $(this).css("opacity", "0.3");
+  $(this).addClass("reminded");
 });
+
+$("#nextWeek").click(function() {
+  $("#calendarRow span").removeClass("today");
+
+  let nextWeek = $("#calendarRow td:nth-child(1) span").text();
+  nextWeek = parseInt(nextWeek) + 7;
+
+  for (let i = 1; i < 8; i++) {
+    let id = "#calendarRow td:nth-child("+ i +") span";
+    $(id).text(nextWeek);
+
+    if (nextWeek == date) {
+      $(id).addClass("today");
+    }
+
+    nextWeek++;
+  }
+});
+
+$("#prevWeek").click(function () {
+  $("#calendarRow span").removeClass("today");
+
+  let nextWeek = $("#calendarRow td:nth-child(1) span").text();
+  nextWeek = parseInt(nextWeek) - 7;
+
+  for (let i = 1; i < 8; i++) {
+    let id = "#calendarRow td:nth-child("+ i +") span";
+    $(id).text(nextWeek);
+
+    if (nextWeek == date) {
+      $(id).addClass("today");
+    }
+    nextWeek++;
+  }
+})
