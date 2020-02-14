805b392ca1c0561990e19b044508781f51acf9f3
Jessica Nguyen <jdn039@ucsd.edu>
2020-02-13 17:07:20 -0800

    working?

---
+++ public/js/chores.js
@@ -0,0 +1,169 @@
+'use strict';
+
+var choreList;
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+  $.getJSON("https://api.myjson.com/bins/d49qw", function(json) {
+    highlightChores(json);
+ });
+
+  $("#addChoreButton").click(function() {
+    $("#addChore").modal("show");
+  });
+
+  $(".removeChore").click(function () {
+    $(this).parent().parent().fadeOut(300);
+  });
+
+  $("#add").click(function () {
+    let choreName = $("#choreName").val();
+    let imageURL = $("input[name='icon']:checked").val();
+    console.log(choreName);
+    console.log(imageURL);
+    let freq = [];
+    let assigned = [];
+    let id = Object.keys(choreList.chores).length;
+    id = "a" + id;
+    console.log(id);
+
+    $.each($("input[name='day']:checked"), function() {
+      freq.push($(this).val());
+    });
+
+    $.each($("input[name='roomie']:checked"), function() {
+      assigned.push($(this).val());
+    });
+
+    console.log(freq);
+    console.log(assigned);
+
+    console.log(choreList);
+
+    var newChore = {
+      "name": choreName,
+      "id": id,
+      "iconURL": imageURL,
+      "frequency": freq,
+      "assigned": assigned,
+      "completed": false
+    };
+
+    choreList.chores.push(newChore);
+    console.log(choreList);
+
+    createChore(newChore);
+    highlightChores(choreList);
+/*
+    var data = JSON.stringify(choreList);
+
+    $.ajax({
+        url: "https://api.myjson.com/bins/d49qw",
+        type: "POST",
+        data: data,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data, textStatus, jqXHR) {
+
+            // do update
+            $.ajax({
+                url: "https://api.myjson.com/bins/d49qw",
+                type: "PUT",
+                data: data,
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function (data, textStatus, jqXHR) {
+                    console.log("UPDATED!!!");
+                    highlightChores();
+                }
+            });
+        }
+    });
+    */
+    $("#addChore").modal("hide");
+  });
+});
+
+function createChore(chore) {
+  let table = "<table class='table table-borderless' style='margin: 0 0 0 0.5rem;'><tbody><tr><td scope='row'><span>S</span></td><td><span class=''>M</span></td><td><span class=''>T</span></td><td><span class=''>W</span></td><td><span class=''>T</span></td><td><span class=''>F</span></td><td><span>S</span></td></tr></tbody></table>";
+  let assign = "<p class='assigned'>Assigned to:<img class='profile-chores' src='/images/profile.png' alt='profile'><img class='profile-chores' src='/images/profile.png' alt='profile'><img class='profile-chores' src='/images/profile.png' alt='profile'><img class='profile-chores' src='/images/profile.png' alt='profile'></p>";
+
+  let element = "<div class='chore'><div class='chore-card-2'><img class='removeChore' src='/images/close.svg' alt='close' width='20'><img class='profile-icon' src='" + chore.iconURL + "' alt='clean'><div id='"+ chore.id +"'><p><strong>"+ chore.name + "</strong></p>"+ table + assign + "</div></div><hr></div>";
+  $(".newChore").append(element);
+}
+
+function highlightChores(json) {
+  console.log(json);
+  choreList = json;
+
+  for (let idx in json["chores"]) {
+    let currentChore = json["chores"][idx];
+    let id = currentChore["id"];
+
+    // Iterate through list of assignees to properly highlight the chore page
+    for (let i in currentChore["assigned"]) {
+
+      if (currentChore["assigned"][i] == "A") {
+        id = "#"+id+" .assigned img:nth-child(1)";
+        $(id).addClass("active-icon");
+        id = currentChore["id"];
+      }
+
+      if (currentChore["assigned"][i] == "B") {
+        id = "#"+id+" .assigned img:nth-child(2)";
+        $(id).addClass("active-icon");
+        id = currentChore["id"];
+      }
+
+      if (currentChore["assigned"][i] == "C") {
+        id = "#"+id+" .assigned img:nth-child(3)";
+        $(id).addClass("active-icon");
+        id = currentChore["id"];
+      }
+
+      if (currentChore["assigned"][i] == "D") {
+        id = "#"+id+" .assigned img:nth-child(4)";
+        $(id).addClass("active-icon");
+        id = currentChore["id"];
+      }
+    }
+
+    // Iterate through frequency list to appropriately mark date
+    for (let j in currentChore["frequency"]) {
+       if (currentChore["frequency"][j] == "Sun") {
+         id = "#"+id+" td:nth-child(1) span";
+         $(id).addClass("active-date today");
+         id = currentChore["id"];
+       }
+       if (currentChore["frequency"][j] == "Mon") {
+         id = "#"+id+" td:nth-child(2) span";
+         $(id).addClass("active-date today");
+         id = currentChore["id"];
+       }
+       if (currentChore["frequency"][j] == "Tue") {
+         id = "#"+id+" td:nth-child(3) span";
+         $(id).addClass("active-date today");
+         id = currentChore["id"];
+       }
+       if (currentChore["frequency"][j] == "Wed") {
+         id = "#"+id+" td:nth-child(4) span";
+         $(id).addClass("active-date today");
+         id = currentChore["id"];
+       }
+       if (currentChore["frequency"][j] == "Thu") {
+         id = "#"+id+" td:nth-child(5) span";
+         $(id).addClass("active-date today");
+         id = currentChore["id"];
+       }
+       if (currentChore["frequency"][j] == "Fri") {
+         id = "#"+id+" td:nth-child(6) span";
+         $(id).addClass("active-date today");
+         id = currentChore["id"];
+       }
+       if (currentChore["frequency"][j] == "Sat") {
+         id = "#"+id+" td:nth-child(7) span";
+         $(id).addClass("active-date today");
+         id = currentChore["id"];
+       }
+    }
+  }
+}
