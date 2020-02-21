18bd715c14a22ceb52fbf4fade080294164fac2e
Jessica Nguyen <jdn039@ucsd.edu>
2020-02-20 23:44:44 -0800

    trade works

--- public/js/trade.js
+++ public/js/trade.js
@@ -5,9 +5,22 @@ $(document).ready(function() {
      if ($(this).css("border-left-color") == color) {
       $(this).css("border", "2px solid #04B315");
        console.log("already clicked");
+       $(this).addClass("select");
      }
      else {
        $(this).css("border", "2px solid #F1F1F1");
+       $(this).removeClass("select");
      }
+ });
+
+ $("#selected").click(function () {
+   alert("You've accepted the trade!");
+   $(".select").remove();
+ });
+
+ $("#decline").click(function () {
+   alert("You've declined the trade!");
+   $(".select").css("border", "2px solid #F1F1F1");
+   $(".select").removeClass("select");
  });
 });
